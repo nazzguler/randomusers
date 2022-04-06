765a93a137665853308bd2d0a56e93f576490e22
Oscar Posadas <nazzguler@hotmail.com>
2022-04-06 14:43:51 -0500

    adding single observer and fetching data

--- app/src/main/java/com/example/randomusers/viewmodel/RandomUsersViewModel.kt
+++ app/src/main/java/com/example/randomusers/viewmodel/RandomUsersViewModel.kt
@@ -1,8 +1,12 @@
 package com.example.randomusers.viewmodel
 
 import androidx.lifecycle.ViewModel
+import com.example.randomusers.model.RandomUsersResponse
 import com.example.randomusers.network.RandomUsersRepository
 import com.example.randomusers.view.RandomUsersView
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.observers.DisposableSingleObserver
+import io.reactivex.schedulers.Schedulers
 
 class RandomUsersViewModel(
     val randomUsersView: RandomUsersView,
@@ -10,6 +14,23 @@ class RandomUsersViewModel(
 ) : ViewModel() {
 
     fun getRandomUsers(results: Int) {
+        randomUsersView.onLoading()
         randomUsersRepository.getRandomUsersByResults(results)
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe(getRandomUsersSingleObserver())
+    }
+
+    private fun getRandomUsersSingleObserver(): DisposableSingleObserver<RandomUsersResponse> {
+        return object : DisposableSingleObserver<RandomUsersResponse>() {
+
+            override fun onSuccess(response: RandomUsersResponse) {
+                randomUsersView.onSuccess(response)
+            }
+
+            override fun onError(error: Throwable) {
+                randomUsersView.onFailure(error.toString())
+            }
+        }
     }
 }
