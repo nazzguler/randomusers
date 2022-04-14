6a126b8497bc6f6421bb4f8906b3886b89ba6f29
Oscar Posadas <nazzguler@hotmail.com>
2022-04-14 11:12:16 -0500

    removing useless rxjava adapter factory when using coroutine

--- app/src/main/java/com/example/randomusers/network/RetrofitManager.kt
+++ app/src/main/java/com/example/randomusers/network/RetrofitManager.kt
@@ -1,7 +1,6 @@
 package com.example.randomusers.network
 
 import retrofit2.Retrofit
-import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
 import javax.inject.Inject
 
@@ -14,7 +13,6 @@ class RetrofitManager @Inject constructor() {
             return Retrofit.Builder()
                 .baseUrl(randomUsersApi)
                 .addConverterFactory(GsonConverterFactory.create())
-                .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
                 .build()
         }
     }
